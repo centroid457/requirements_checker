26e8e6eafab11af8051585d99c056c6b96cc7ac3
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-01 17:37:53 +0300

    finish tests

--- _test.py
+++ _test.py
@@ -9,18 +9,91 @@ from requirements_checker import *
 
 # =====================================================================================================================
 class Test_Base:
-    def test__exx_value(self):
-        class Victim(ReqCheckStr_Base):
-            _GETTER = lambda: "Hello"
-            _RAISE = True
-            _CHECK_FULLMATCH = True
+    def setup_method(self, method):
+        self.VICTIM = type("VICTIM", (ReqCheckStr_Base,), {})
 
-        victim = Victim()
+    # -----------------------------------------------------------------------------------------------------------------
+    def test__exx_no_getter(self):
+        try:
+            victim = self.VICTIM()
+        except Exx_RequirementCantGetActualValue:
+            assert True
+        else:
+            assert False
+
+    def test__no_reqs(self):
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM._RAISE = False
+        victim = self.VICTIM()
+
+        assert victim.check() is False
+        assert victim._VALUE_ACTUAL == "hello"
+
+    def test__req_met_true(self):
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM.hello = True
+        victim = self.VICTIM()
 
         assert victim.check() is True
-        assert victim.VALUE_ACTUAL == "hello"
 
-        # victim.
+    def test__req_met_true__several_variants(self):
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM.hello1 = False
+        self.VICTIM.hello = True
+        self.VICTIM.hello2 = False
+        victim = self.VICTIM()
+
+        assert victim.check() is True
+
+    def test__req_met_false(self):
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM.hello = False
+        try:
+            victim = self.VICTIM()
+        except Exx_Requirement:
+            assert True
+        else:
+            assert False
+
+    def test__set_raise(self):
+        # _RAISE = True
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM._RAISE = True
+        self.VICTIM.hello = False
+        try:
+            victim = self.VICTIM()
+        except Exx_Requirement:
+            assert True
+        else:
+            assert False
+
+        # _RAISE = False
+        self.VICTIM._RAISE = False
+        victim = self.VICTIM()
+        assert victim.check() is False
+
+    def test__set_part(self):
+        self.VICTIM._RAISE = False
+        self.VICTIM._GETTER = lambda: "Hello"
+
+        # _CHECK_FULLMATCH = True
+        self.VICTIM._CHECK_FULLMATCH = True
+        self.VICTIM.hell = True
+
+        victim = self.VICTIM()
+        # victim.hello
+        assert victim.check() is False
+
+        # _CHECK_FULLMATCH = False
+        self.VICTIM._CHECK_FULLMATCH = False
+        assert victim.check() is True
+
+    def test__case_insencitive(self):
+        self.VICTIM._GETTER = lambda: "Hello"
+        self.VICTIM.HELLO = True
+        victim = self.VICTIM()
+
+        assert victim.check() is True
 
 
 # =====================================================================================================================
--- requirements_checker/main.py
+++ requirements_checker/main.py
@@ -23,7 +23,7 @@ class ReqCheckStr_Base:
     VARIANTS for check
     ------------------
     add attributes with bool values (case-insensitive)
-        - True - if value is definitely acceptable
+        - True - if value is definitely acceptable - Always need at least one True!!!
         - False - if not definitely acceptable
         - None - if requirement is undefined
 
@@ -35,12 +35,12 @@ class ReqCheckStr_Base:
         False - if partial match by finding mentioned values in value actual
 
     :ivar _GETTER: function which will get the exact value to check
-    :ivar VALUE_ACTUAL:
+    :ivar _VALUE_ACTUAL:
     """
     _GETTER: Callable = None
     _RAISE: bool = True
     _CHECK_FULLMATCH: bool = True
-    VALUE_ACTUAL: Optional[str] = None
+    _VALUE_ACTUAL: Optional[str] = None
 
     def __init__(self):
         self.check()
@@ -52,31 +52,34 @@ class ReqCheckStr_Base:
 
         try:
             value = self.__class__._GETTER()
-            self.VALUE_ACTUAL: str = value.lower()
+            self._VALUE_ACTUAL: str = value.lower()
         except Exception as exx:
             raise Exx_RequirementCantGetActualValue(repr(exx))
 
+        msg = "[ERROR] No TRUE variants MET!"
         for name in dir(self):
             if name.startswith("_"):
                 continue
             acceptance: Optional[bool] = getattr(self, name)
-            if isinstance(acceptance, bool):
-                msg = f"[ERROR] requirement not ACCEPTABLE [{self.__class__.__name__}/{self.VALUE_ACTUAL=}/req={name}]"
+            name = name.lower()
+            if acceptance is True:
+                msg = f"[ERROR] requirement not ACCEPTABLE [{self.__class__.__name__}/{self._VALUE_ACTUAL=}/req={name}]"
                 if (
-                        (self._CHECK_FULLMATCH and name == self.VALUE_ACTUAL)
+                        (self._CHECK_FULLMATCH and name == self._VALUE_ACTUAL)
                         or
-                        (not self._CHECK_FULLMATCH and name in self.VALUE_ACTUAL)
+                        (not self._CHECK_FULLMATCH and name in self._VALUE_ACTUAL)
                 ):
-                    if self._RAISE:
-                        raise Exx_Requirement(msg)
-                    else:
-                        return acceptance
+                    return True
+
         # final
-        return True
+        if self._RAISE:
+            raise Exx_Requirement(msg)
+        else:
+            return False
 
 
 # =====================================================================================================================
-class ReqChecStr_Os(ReqCheckStr_Base):
+class ReqCheckStr_Os(ReqCheckStr_Base):
     pass
 
 
