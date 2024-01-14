bc51ff92f551c17f8d947e1dc4451c9830be0f1a
admin <023>
2024-01-14 04:42:31 +0300

    ready v0.1.0

---
+++ PROJECT.py
@@ -0,0 +1,60 @@
+from typing import *
+
+
+# =====================================================================================================================
+class PROJECT:
+    # AUX --------------------------------------------------
+    _VERSION_TEMPLATE: Tuple[int] = (0, 0, 1)
+
+    # AUTHOR -----------------------------------------------
+    AUTHOR_NAME: str = "Andrei Starichenko"
+    AUTHOR_EMAIL: str = "centroid@mail.ru"
+    AUTHOR_HOMEPAGE: str = "https://github.com/centroid457/"
+
+    # PROJECT ----------------------------------------------
+    NAME_INSTALL: str = "requirements-checker"
+    NAME_IMPORT: str = "requirements_checker"
+    KEYWORDS: List[str] = [
+        "check requirements", "check system requirements", "raise/bool if no requirements",
+    ]
+
+    # GIT --------------------------------------------------
+    DESCRIPTION_SHORT: str = "check if requirements met"
+
+    # README -----------------------------------------------
+    pass
+
+    # add DOUBLE SPACE at the end of all lines! for correct representation in MD-viewers
+    DESCRIPTION_LONG: str = """
+designed for check requirements (systemOs) and raise/bool if no match
+    """
+    FEATURES: List[str] = [
+        # "feat1",
+        # ["feat2", "block1", "block2"],
+
+        "check requirements (systemOs), raise/bool if no match",
+        "create fuck/getter and is it for check for settings",
+        "...see tests for this!"
+    ]
+
+    # HISTORY -----------------------------------------------
+    VERSION: Tuple[int, int, int] = (0, 1, 0)
+    VERSION_STR: str = ".".join(map(str, VERSION))
+    TODO: List[str] = [
+        "add check_version (py interpreter for example!)",
+        "add work with packages",
+    ]
+    FIXME: List[str] = [
+        "..."
+    ]
+    NEWS: List[str] = [
+        "apply new pypi template"
+    ]
+
+
+# =====================================================================================================================
+if __name__ == '__main__':
+    pass
+
+
+# =====================================================================================================================
