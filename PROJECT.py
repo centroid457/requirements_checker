97baf6c9b6f7c8abfd15ec4fc3abb22a723db561
Andrei Starichenko <centroid@mail.ru>
2024-05-22 15:31:43 +0300

    ready v0.1.13

--- PROJECT.py
+++ PROJECT.py
@@ -41,7 +41,7 @@ designed for check requirements (systemOs) and raise/bool if no match
     ]
 
     # HISTORY -----------------------------------------------
-    VERSION: Tuple[int, int, int] = (0, 1, 12)
+    VERSION: Tuple[int, int, int] = (0, 1, 13)
     TODO: List[str] = [
         "add WARN_if__*/if_not__* (and use message in stderr)",
         "add check_version (py interpreter for example!)",
@@ -51,7 +51,8 @@ designed for check requirements (systemOs) and raise/bool if no match
         "sometimes modules have incorrect SHARE!!! maybe need check upgrade after installation!!! and show ERROR!"
     ]
     NEWS: List[str] = [
-        "[Packages] add upgrade_prj",
+        "[__INIT__.py] fix import",
+        "apply last pypi template",
     ]
 
     # FINALIZE -----------------------------------------------
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# requirements_checker (v0.1.12)
+# requirements_checker (v0.1.13)
 
 ## DESCRIPTION_SHORT
 check if requirements met
